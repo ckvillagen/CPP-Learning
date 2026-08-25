d8143141dba7407aaefef757de1b3be8bf421b20
CK Villagen <ckrulidavillagen@gmail.com>
2026-08-25 16:49:46 +0800

    My C++ learning progress

---
+++ Age.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int age = 18;
+    double height = 5.9;
+    char grade = 'A';
+    string name = "CK";
+    bool student = true;
+    cout << name << endl;
+    cout << age << endl;
+    cout << height << endl;
+    cout << grade << endl;
+    cout << student << endl;
+    return 0;
+}
---
+++ data.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int age = 18;
+    double height = 5.9;
+    char grade = 'A';
+    string name = "CK";
+    bool student = true;
+    cout << name << endl;
+    cout << age << endl;
+    cout << height << endl;
+return 0;
+}
